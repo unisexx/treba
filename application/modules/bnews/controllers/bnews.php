d540c8623b6767064e6f420e1e9d96c6bc49c01a
unisexx <unisexx@hotmail.com>
2013-10-21 07:13:34 +0700

    zzz

--- application/modules/bnews/controllers/bnews.php
+++ application/modules/bnews/controllers/bnews.php
@@ -8,13 +8,13 @@ class Bnews extends Public_Controller
     
     function index(){
         $data['bnews'] = new Bnew();
-        $data['bnews']->order_by('id','asc')->get_page();
+        $data['bnews']->order_by('id','desc')->get_page();
         $this->template->build('index',$data);
     }
     
     function inc_home(){
         $data['bnews'] = new Bnew();
-        $data['bnews']->order_by('id','asc')->get(6);
+        $data['bnews']->order_by('id','desc')->get(6);
         $this->load->view('inc_home',$data);
     }
     
